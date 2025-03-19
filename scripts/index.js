f1a8927bbd6f5a46e2bd7825727b5ffc2f60818a
lensboole <lensboole@yandex.ru>
2025-03-19 16:50:57 +0300

    попытка-1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function addCard({ link, name }, deleteClick) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__image").alt = name;
+  cardElement.querySelector(".card__title").textContent = name;
+
+  deleteButton.addEventListener("click", deleteClick);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  const card = event.target.closest(".card");
+  card.remove();
+}
+
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  const cardElement = addCard(card, deleteCard);
+  cardsContainer.append(cardElement);
+});
