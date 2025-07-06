28bb35e7e6f826a1a273f14fcabc490607c527ea
lensboole <lensboole@yandex.ru>
2025-07-06 10:18:02 +0300

    попытка 3

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,13 +1,13 @@
 export function openPopup(popup) {
   popup.classList.add("popup_is-opened");
   document.addEventListener("keydown", closeEscPopup);
-  popup.addEventListener("mousedown", closeOnOverlayClick);
+  popup.addEventListener("mousedown", handleOverlayClick);
 }
 
 export function closePopup(popup) {
   popup.classList.remove("popup_is-opened");
   document.removeEventListener("keydown", closeEscPopup);
-  popup.removeEventListener("mousedown", closeOnOverlayClick)
+  popup.removeEventListener("mousedown", handleOverlayClick)
 }
 
 function closeEscPopup(evt) {
@@ -16,5 +16,11 @@ function closeEscPopup(evt) {
     if (openedPopup) {
       closePopup(openedPopup);
     }
+  }
+}
+
+function handleOverlayClick(evt) {
+  if (evt.target === evt.currentTarget) {
+    closePopup(evt.currentTarget);
   }
 }
